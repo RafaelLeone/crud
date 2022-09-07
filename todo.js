f2bd93a23fba50c18dda34e2679afa4fb1869fdf
RafaelLeone <rinaciodacosta@gmail.com>
2022-09-07 19:30:29 -0300

    first commit

---
+++ todo.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "Olá Vue!",
+        tasks: [
+
+        ],
+    },
+    methods: {
+        remove: function(tarefa){
+            let index = this.tasks.indexOf(tarefa);
+            this.tasks.splice(index, 1);
+        }
+    },
+    created(){
+        console.log('entrou aqui')
+            fetch('http://localhost:3000/tasks')
+            .then((response) => response.json())
+            .then((tasks) =>
+                (this.tasks = tasks))
+            },
+        })
