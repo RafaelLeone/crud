7c3e14aac5c3a88221e91ff01ede082178d330d1
RafaelLeone <rinaciodacosta@gmail.com>
2022-09-07 22:07:49 -0300

    crud v1

--- todo.js
+++ todo.js
@@ -1,7 +1,3 @@
-Vue.component("modal", {
-    template: "#modal-template"
-  });
-
 var app = new Vue({
     el: "#app",
     data: {
@@ -9,9 +5,31 @@ var app = new Vue({
         tasks: [
 
         ],
-        showModal: false
+        showModal: false,
+        showModalEdit: false,
+        novaTarefa: '',
+        tagDeCategoria: '',
     },
     methods: {
+        add: function(){
+            console.log(this.tasks.length+1)
+            this.tasks.push({"id": this.tasks.length+1,
+            "title": this.novaTarefa,
+            "dueTo": "2022-05-01T12:00:00.000Z",
+            "project": this.tagDeCategoria,
+            "usuario": "joao@example.com",
+            "feita": ""})
+        },
+        close: function(){
+            this.showModal = false;
+            this.showModalEdit = false;
+        },
+        edit: function(tarefa){
+            tarefa.title = this.novaTarefa
+            tarefa.project = this.tagDeCategoria
+            console.log(this.novaTarefa)
+            console.log(tarefa.title)
+        },
         remove: function(tarefa){
             let index = this.tasks.indexOf(tarefa);
             this.tasks.splice(index, 1);
